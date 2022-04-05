3e8dcdc28af22d3e33f2da0ae5b49618d36ebf36
mwalmer <75793394+mwalmer@users.noreply.github.com>
2022-04-05 09:51:28 -0400

    added the ability to store options in chrom and gui update

--- src/popup.js
+++ src/popup.js
@@ -1,21 +1,27 @@
-// Initialize button with user's preferred color
-let changeColor = document.getElementById("changeColor");
+const selected = document.querySelector('#selected');
+const options = document.querySelectorAll('.option');
 
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
-});
-
-// When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener("click", async () => {
-    addAllEvents();
-  });
+for(let i = 0; i < options.length; i++)
+{
+  options[i].addEventListener('change', (event) => {
+    let key = event.target.name;
+    let value = event.target.value;
   
+    // stores value based on checkbox status
+    // set requires a key/value pair in the form "key": value
+    if(event.target.checked)
+      chrome.storage.sync.set({ [`${key}`]: value });
+    else
+      chrome.storage.sync.set({ [`${key}`]: 'disabled' });
+    
+    UpdateSelected(key);
+  });
+}
 
-
-  // The body of this function will be executed as a content script inside the
-  // current page
-  function setPageBackgroundColor() {
-    chrome.storage.sync.get("color", ({ color }) => {
-      document.body.style.backgroundColor = color;
-    });
-  }
+function UpdateSelected(val) {
+  // gets the value stored
+  // get requires a key - which is a string
+  chrome.storage.sync.get([val], function(result) {
+    selected.innerHTML = `${val} = ` + result[`${val}`];
+  });
+}
