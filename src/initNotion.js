c1c07256c80e2b8434fac20624d7d9a80e23fbb2
mwalmer <75793394+mwalmer@users.noreply.github.com>
2022-04-18 03:32:15 -0400

    formatting

--- src/initNotion.js
+++ src/initNotion.js
@@ -1,15 +1,15 @@
 const options = {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Notion-Version': '2022-02-22',
-      'Content-Type': 'application/json',
-      Authorization: 'Bearer secret_EuaqGyOIWlphmYIjS3gW9d8uUuud6VAOzZriQYkhEZE'
-    },
-    body: JSON.stringify({parent: 'ce1292e3445e420d948a1f1b242c1d87', properties: 'aaa'})
-  };
-  
-  fetch('https://api.notion.com/v1/databases', options)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
+  method: 'POST',
+  headers: {
+    Accept: 'application/json',
+    'Notion-Version': '2022-02-22',
+    'Content-Type': 'application/json',
+    Authorization: 'Bearer secret_EuaqGyOIWlphmYIjS3gW9d8uUuud6VAOzZriQYkhEZE'
+  },
+  body: JSON.stringify({parent: 'ce1292e3445e420d948a1f1b242c1d87', properties: 'aaa'})
+};
+
+fetch('https://api.notion.com/v1/databases', options)
+  .then(response => response.json())
+  .then(response => console.log(response))
+  .catch(err => console.error(err));
