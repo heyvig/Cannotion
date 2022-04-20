0cac28426d3df6a3aac867d4354ef792d964face
Richard Alvero <richiealvero@gmail.com>
2022-04-20 19:39:15 -0400

    Parser fully commented and updated

--- src/parser.js
+++ src/parser.js
@@ -19,7 +19,7 @@ class CalendarEvent{
     hour = 0;
     minute = 0;
     
-    class = "";
+    c = "";
     description = "";
     link = "";
 
@@ -35,7 +35,7 @@ class CalendarEvent{
 
     reset(){
         this.title = "";
-        this.class = "";
+        this.c = "";
         this.type = "";
         this.description = ""; //Prob not doable anymore due to incredibly high amounts of gibberish
         this.link = "";
@@ -49,15 +49,21 @@ class CalendarEvent{
     }
 }
 
+//Generic parsing from a string into an array
 function parse(dataString, events_arr){
 
     //Create new calendar event
     let newEvent = new CalendarEvent();
-    
+    var index = 0;
+
+    //Line by line read of the data string
     const lines = dataString.split('\n').map(function(line){
+        //Checking that the end of the file has not been reached yet
         if(!(line.includes("</feed>"))){
             
+            //Grabbing information about the type and title of the event and updating the respective object variables
             if(line.includes("<title>")){
+                //Updating type of event
                 if(line.includes("Calendar Event:")){
                     if(line.toLowerCase().includes("office hours")){
                         newEvent.type = "Office Hours";
@@ -85,13 +91,16 @@ function parse(dataString, events_arr){
                     newEvent.type = "Assignment";
                 }
                 
+                //Updating the title of the event
                 newEvent.title = line.substring(line.indexOf(":") + 2, line.lastIndexOf("<"));
             }
 
+            //Updating the year of the event
             if(line.includes("<published>")){
                 newEvent.year = line.substring(line.indexOf(">") + 1, line.indexOf(">") + 5);
             }
 
+            //Updating the link for events
             if(line.includes("<link") && !line.includes("rel=\"self\"")){
                 if(!(newEvent.type == "Office Hours" || newEvent.type == "Discussion" || newEvent.type == "Lab" || newEvent.type == "Class")){
                     newEvent.link = line.substring(line.indexOf("http://"), line.lastIndexOf("\"/>"));
@@ -102,11 +111,13 @@ function parse(dataString, events_arr){
                 }
             }
             
+            //Updating the class (ex: "CEN3031")
             if(line.includes("<name>")){
-                newEvent.class = line.substring(line.indexOf(">") + 1, line.lastIndexOf("<"));
+                newEvent.c = line.substring(line.indexOf(">") + 1, line.lastIndexOf("<"));
             }
 
             if(line.includes("<content type")){
+                //Updating the link for zoom conferences and event types
                 if(line.includes("zoom.us/")){
                     var subLine = line.substring(line.indexOf("https://"), line.indexOf("target="));
                     newEvent.link = subLine.substring(0, subLine.indexOf("\""));
@@ -116,6 +127,7 @@ function parse(dataString, events_arr){
                     }
                 }
 
+                //Updating the date and times of the event
                 if(newEvent.type == "Office Hours" || newEvent.type == "Discussion" || newEvent.type == "Lab" || newEvent.type == "Class" || newEvent.type == "Event"){
                     newEvent.month = line.substring(line.indexOf(">") + 1, line.indexOf(">") + 4);
                     newEvent.day = parseInt(line.substring(line.indexOf(">") + 5, line.indexOf(">") + 7));
@@ -187,11 +199,13 @@ function parse(dataString, events_arr){
                 }
             }
 
+            //End of the line for 
             if(line.includes("</entry>")){
                 //PUSH AKA COPY current event (NOT A POINTER) into collection
-                events_arr.push(Object.assign({}, newEvent));
+                events_arr[index] = Object.assign({}, newEvent);
                 //console.log(newEvent.title + " - " + newEvent.class);
                 newEvent.reset();
+                index++;
             }
         }
         else{
@@ -204,18 +218,10 @@ function parse(dataString, events_arr){
 
 function parseFile(file){
     //Create container for events
-    let events_arrF = [];
+    let events_arrF = new Array(160);
 
+    //parsing the file string and storing the events in events_arrF
     events_arrF = parse(file, events_arrF);
-
-    // for(const element of events_arrF){
-    //     console.log(element.title + " | " + element.class + " | " + element.type);
-    //     console.log(element.link);
-    //     console.log("Date: " + element.month + " " + element.day + ", " + element.year);
-    //     console.log("From: " + element.hour + ":" + element.minute);
-    //     console.log("\n");
-    // }
-    // console.log(events_arrF);
 
     if(events_arrF.length > 0)
         return events_arrF;
@@ -223,14 +229,26 @@ function parseFile(file){
 
 function parseLink(link){
     //Create container for events
-    let events_arrL = [];
-    
+    let events_arrL = new Array(160);
+
     //Correcting link file ending
-    var newLink = link.substring(0, link.length - 3);
+    var newLink = link.substring(0, link.length - 4);
     newLink = newLink.concat(".atom");
-    console.log(newLink);
+
+    //Excluding CORS to prevent breakage -Max
+    const headers = {
+        'Accept': '*/*',
+        'User-Agent': 'request',
+    };
+    const options = {
+        url: newLink,
+        headers:  headers
+    };
+
+    //Requesting the HTML from the .atom link and parsing it
     request(link, function(error, response, html){
         if(!error && response.statusCode == 200){
+            //parsing the html string and storing the events in events_arrL
             return parse(html, events_arrL);
         }
         else{
@@ -238,22 +256,8 @@ function parseLink(link){
         }
     });
 
-    // for(element of events_arrL){
-    //     console.log(element.title + " | " + element.class + " | " + element.type);
-    //     console.log(element.link);
-    //     console.log("Date: " + element.month + " " + element.day + ", " + element.year);
-    //     console.log("From: " + element.hour + ":" + element.minute);
-    //     console.log("\n");
-    // }
-
     if(events_arrL.length > 0)
         return events_arrL;
 }
 
-//parseLink('https://ufl.instructure.com/feeds/calendars/user_BkaffhCJl6Sh6F30F7EJ0RvsAWA8arHizxJ4xMus.ics');
-
-// COMMENT THESE OUT WHEN USING NODE
 module.exports = { parseFile, parseLink, CalendarEvent};
-
-// let events_arr = parseFile(test_file);
-// console.log(events_arr.length);
